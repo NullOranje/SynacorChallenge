b34bb3cc460c3ca98b5e01de3be81521d3bf5f71
Nick McKinney <nick@ndmckinney.com>
2016-06-05 19:24:32 -0700

    Added shell for reference monitor.

---
+++ src/Monitor.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nicholas on 6/5/16.
+ */
+public class Monitor {
+    // TODO: Implement reference monitor to interact with VM without using debugger
+
+    public Monitor() {
+
+    }
+}
